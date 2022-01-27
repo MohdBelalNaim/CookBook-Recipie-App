a1017a547855df5e7d1e5f1b7f10e9e69de6d0aa
unknown <contactbelalnaim@gmail.com>
2022-01-27 23:46:21 +0530

    Added fetch from auth.id still need to fix a few bugss i fetching

--- src/Pages/My-recipies.jsx
+++ src/Pages/My-recipies.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import MyItem from '../components/My-items';
-import Navbar from '../components/Navbar'
+import {collection,query,getDocs,where} from 'firebase/firestore'
+import {db,auth} from '../firebase'
 
 const MyRecipes = () =>{
     const navigate = useNavigate()
@@ -11,23 +12,34 @@ const MyRecipes = () =>{
         }
     }
     checkLogin()
+
+    
+    const[myItems,setMyItems]=useState([])
+
+    const ref = collection(db,"recipies")
+    const q = query(ref,where("user.id","==",auth.currentUser.uid))
+    useEffect(()=>{
+        const getData = async() =>{
+            const data = await getDocs(q)
+            setMyItems(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
+        }
+        getData()
+    })
+
+
+
     return(
         <>
             <div className='py-2 px-3 h4'><b>My recipies</b></div>
             <div className='container'>
                 <div className='row'>
-                <MyItem/> 
-                <MyItem/>   
-                <MyItem/> 
-                <MyItem/> 
-                <MyItem/> 
-                <MyItem/> 
-                <MyItem/> 
-                <MyItem/> 
-                <MyItem/> 
-                <MyItem/> 
-                <MyItem/> 
-                <MyItem/> 
+                {
+                    myItems.map(item=>{
+                        return(
+                            <MyItem name={item.name} image={item.image} />
+                        )
+                    })
+                }
                 </div>
             </div>
         </>
--- src/components/My-items.jsx
+++ src/components/My-items.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
 
-const MyItem=()=>{
+import React from 'react';
+const MyItem=({name,image})=>{
+
     return(
             <div className='col-lg-4 py-2 px-2 animate__animated animate__slideInUp'>
                <div className='border border-dark'>
                <div className='row'>
-                    <div className='col-lg-5 col-5'>
-                        <img className='img-fluid' src="https://parade.com/wp-content/uploads/2020/06/Best-pasta-recipes.jpg" alt="" />
+                    <div className='col-lg-5 col-5' style={{"height":150+"px"}}>
+                        <img className='img-fluid' src={image} alt={name} style={{"height":100+"%","width":100+"%","objectFit":"cover"}}  />
                     </div>
                     <div className='col-lg-7 py-3 col-7'>
-                        <div>White sauce pasta</div>
+                        <div>{name}</div>
                         <div>
                             <button className='btn btn-danger mt-3' style={{"fontSize":16+"px"}}> <i className='fas fa-trash'></i> Remove</button>
                         </div>
